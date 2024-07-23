be78e3ba33402cccbe3a7568c5e71c33e3aa6a93
thenotoriousxul <saulsanchezlopez999@gmail.com>
2024-07-23 14:18:11 -0600

    agregue la vista de servicios, la vista de la informacion de los empleados, hice cambios en el menu principal y agregue el menu de vehiculos

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,45 +1,54 @@
 <script setup>
-
+import { ref } from 'vue';
 </script>
 
 <template>
+  <div class="page-container">
     <div class="contenedor">
- <v-container class="fill-height">
-<v-row>
-<v-col cols="6" offset="3">
-    <v-card width="50vw">
-    <v-card-title class="bg-red-darken-4" style="text-align: center;">Inicia Sesion</v-card-title>
-    <v-card-text>
-        <v-container>
-            <v-row>
-                <v-col cols="3" >Usuario</v-col>
-                <v-col><v-text-field label="Koe" variant="outlined"></v-text-field></v-col>
-            </v-row>
-            <v-row>
-                <v-col cols="3">Contraseña</v-col>
-                <v-col><v-text-field label="password123" variant="outlined"></v-text-field></v-col>
-            </v-row>
-        </v-container>
-    </v-card-text>
-    <v-card-actions>
-        <v-container>
-            <v-row>
-            <v-col class="d-flex" >
-                <v-btn color="primary" block>Ingresar</v-btn>
-            </v-col>
-            <v-col>
-                <v-btn color="primary" block><router-link to="/RegistroClientes">Registrarse</router-link></v-btn>
-            </v-col>
+      <v-container class="fill-height">
+        <v-row>
+          <v-col cols="6" offset="3">
+            <v-card width="50vw">
+              <v-card-title class="bg-red-darken-4" style="text-align: center;">Inicia Sesión</v-card-title>
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col cols="3">Usuario</v-col>
+                    <v-col>
+                      <v-text-field label="Koe" variant="outlined"></v-text-field>
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                    <v-col cols="3">Contraseña</v-col>
+                    <v-col>
+                      <v-text-field label="password123" variant="outlined" type="password"></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+              <v-card-actions>
+                <v-container>
+                  <v-row>
+                    <v-col class="d-flex">
+                      <v-btn color="primary" block>Ingresar</v-btn>
+                    </v-col>
+                    <v-col>
+                      <router-link to="/RegistroClientes">
+                        <v-btn color="primary" block>Registrarse</v-btn>
+                      </router-link>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-actions>
+            </v-card>
+          </v-col>
         </v-row>
-        </v-container>
-    </v-card-actions>
-    </v-card>
-</v-col>
-</v-row>
-</v-container>
-</div>
-<router-view/>
+      </v-container>
+    </div>
+    <router-view />
+  </div>
 </template>
+
 
 <style>
 
---
+++ src/views/ServiciosView.vue
@@ -0,0 +1,28 @@
+
+<script setup>
+
+</script>
+
+<template>
+        <v-app>
+    <v-app-bar app color="#7d0100" dark>
+        <router-link to="/Vehiculos">
+        <v-btn
+            class="ma-3"
+            color="white"
+            icon="mdi-arrow-left-bold-circle-outline"
+        ></v-btn>
+        </router-link>
+        <h1 class="text-center w-100">SERVICIOS</h1>
+    </v-app-bar>
+
+    <v-main>
+        <v-container>
+        <v-data-table :items="datos" :headers="headers" class="elevation-1"></v-data-table>
+        </v-container>
+    </v-main>
+</v-app>
+</template>
+
+<style scoped>
+</style>
